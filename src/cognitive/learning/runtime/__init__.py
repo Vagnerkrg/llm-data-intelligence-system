6ceb100ecadd1b488bb68b308ad0c806a30b89f5
Vagner <vagnerkrg@gmail.com>
2026-07-29 16:38:31 -0300

    feat: integrate cognitive learning runtime layer v1.22

---
+++ src/cognitive/learning/runtime/__init__.py
@@ -0,0 +1,13 @@
+from .learning_execution_context import (
+    LearningExecutionContext
+)
+
+from .learning_runtime_adapter import (
+    LearningRuntimeAdapter
+)
+
+
+__all__ = [
+    "LearningExecutionContext",
+    "LearningRuntimeAdapter",
+]
