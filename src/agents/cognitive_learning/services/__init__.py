664d30fb018ea7d9d4c1d367c5a29f893131e20b
Vagner <vagnerkrg@gmail.com>
2026-08-18 01:14:10 -0300

    feat: implementa cognitive learning loop #67

--- src/agents/cognitive_learning/services/__init__.py
+++ src/agents/cognitive_learning/services/__init__.py
@@ -1,7 +1,17 @@
-from .learning_outcome_engine import LearningOutcomeEngine
-from .learning_signal_processor import LearningSignalProcessor
+from .cognitive_learning_loop import (
+    CognitiveLearningLoop,
+    CognitiveLearningLoopResult,
+)
+from .learning_outcome_engine import (
+    LearningOutcomeEngine,
+)
+from .learning_signal_processor import (
+    LearningSignalProcessor,
+)
 
 __all__ = [
+    "CognitiveLearningLoop",
+    "CognitiveLearningLoopResult",
     "LearningOutcomeEngine",
     "LearningSignalProcessor",
 ]
