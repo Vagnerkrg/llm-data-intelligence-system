ebb516d0c1ca98e66fb1a2246af906c613f015b3
Vagner <vagnerkrg@gmail.com>
2026-08-18 01:20:11 -0300

    feat: integra cognitive learning com agent runtime #68

---
+++ src/agents/runtime/__init__.py
@@ -0,0 +1,9 @@
+"""Agent runtime package."""
+
+from .agent_runtime import AgentRuntime
+from .execution_context import ExecutionContext
+
+__all__ = [
+    "AgentRuntime",
+    "ExecutionContext",
+]
